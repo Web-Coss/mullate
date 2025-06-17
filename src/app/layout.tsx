d456d2580fabd3b2bfd8423c25ac14da7a924079
박상민 <a01071397193@gmail.com>
2025-06-17 20:13:38 +0900

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "src/styles/global.style.scss";
 export const metadata: Metadata = {
     title: "모라떼",
     description: "누구에게나 쉽고 간편한 카페 키오스크",
+    icons: {
+        icon: "/icons/logo.svg"
+    }
 };
 
 export default function RootLayout({children}: Readonly<{
